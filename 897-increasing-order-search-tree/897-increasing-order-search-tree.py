611c6660e5d3689eb693cf2cf44fb3755d605fb4
Lee Dong Wook <sh95119@gmail.com>
2022-04-17 11:23:01 +0900

    Time: 52 ms (29.90%), Space: 14.1 MB (13.79%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.py
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.py
@@ -6,18 +6,19 @@
 #         self.right = right
 class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        tree_nodes = []
         def traverse(node):
             if not node:
                 return
             traverse(node.left)
-            tree_nodes.append(TreeNode(val=node.val))
+            node.left = None
+            self.cur.right = node
+            self.cur = node
             traverse(node.right)
+
+        ans = self.cur = TreeNode(None)
         traverse(root)
+        return ans.right
+        
+        
                         
-        for idx, node in enumerate(tree_nodes):
-            if idx == len(tree_nodes) - 1:
-                break
-            node.right = tree_nodes[idx + 1]
-        return tree_nodes[0] 
         
